e06ac14c762fa397431c6f932addedbe0b3c41c2
Christian <christianmoryah@gmail.com>
2021-09-15 06:45:22 -0300

    update about

--- src/components/modules/about/About.vue
+++ src/components/modules/about/About.vue
@@ -5,25 +5,24 @@
                 <template v-slot:cardText>
                     <div class="black--text">
                         <p>
-                            Assis é um assistente virtual que ajuda gestores públicos a modernizar suas organizações com
-                            uma consultoria especializada sobre ética, inteligência artificial e inovação
-                            na administração pública.
-                        </p>
-                        <p>
-                            Desenvolvido numa parceria entre Oficina Municipal e Fundação Konrad Adenauer, o Assis
-                            usa as tecnologias mais modernas para trazer essas informações de uma forma completamente gratuita.
+                            Desenvolvido por uma parceria entre <b>Oficina Municipal</b> e <b>Fundação Konrad Adenauer</b>, o Assis é um 
+                            professor virtual que ensina gestores públicos como modernizar sua administração, instruindo e 
+                            tirando dúvidas sobre inovação na gestão pública, com uso de inteligência artificial, 
+                            observando princípios éticos e diretrizes legais.
                         </p>
                         <p>
                             Além disso, é possível avaliar o desempenho do Assis como assistente. Sua avaliação é recebida
-                            por um time de especialistas que ajustam o aprendizado do Assis para garantir que ele está dando
-                            as respostas certas para ajudar nos seus problemas mais difíceis.
+                            por um time de especialistas que ajustam o aprendizado do Assis para garantir que ele está 
+                            dando as respostas corretas para ajudar em seus problemas mais difíceis.
                         </p>
                         <p>
-                            Coisas que o Assis sabe falar:
+                            Do que o Assis sabe falar:
                             <ul>
-                                <li>O que é inteligência artificial? Como funciona?</li>
-                                <li>Quais são exemplos de projetos de IA na administração pública?</li>
-                                <li>Quais são as leis relacionadas a esse assunto?</li>
+                                <li>O que é inteligência artificial e como funciona</li>
+                                <li>Como implementar inteligência artificial</li>
+                                <li>Como aplicar inteligência artificial em diferentes municípios</li>
+                                <li>Que exemplos de projetos de inteligência artificial existem na administração pública</li>
+                                <li>Que leis e diretrizes estão relacionadas a esse tipo de inovação</li>
                             </ul>
                         </p>
                         <p>
