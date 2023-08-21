e8e9d1aba14cd8c5f755ae61b62524b13d8a658a
chris-redfield <christianmoryah@gmail.com>
2023-08-21 15:42:04 -0300

    add new beta page

---
+++ src/components/modules/beta/Beta.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <v-container style="text-align: -webkit-center;">
+            <Card :title="'Beta'">
+                <template v-slot:cardText>
+                    <div class="black--text">
+                        <p>
+                            Assis GPT beta test 2
+                        </p>                        
+                        
+                    </div>
+                    <div>
+                        
+                    </div>
+
+                </template>
+            </Card>
+        </v-container>
+        <!-- <flowise-fullchatbot></flowise-fullchatbot> -->
+    </div>
+    
+</template>
+
+<script>
+
+    import Card from '../commom/Card.vue';
+    export default {
+        name: 'About',
+        components: {
+            Card,
+        },
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
